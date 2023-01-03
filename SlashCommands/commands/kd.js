4d3aad943a67bdd28840d8f2d35d1f714d95982f
Alex Liao <anonymousaaaa41414141@gmail.com>
2023-01-04 03:15:13 +0800

    Add level progress bar
    Add
    -User can now choose whish target they want to look at
    -User can now decide whether the kd should be visiable to everyone

--- SlashCommands/commands/kd.js
+++ SlashCommands/commands/kd.js
@@ -8,87 +8,128 @@ const manager = new WBmanager();
 export default {
     data: new SlashCommandBuilder()
         .setName("kd")
+        .addMentionableOption(option =>
+            option
+                .setName("target")
+                .setDescription("The target user you want to look at")
+                .setRequired(false)
+        )
+        .addBooleanOption(option =>
+            option
+                .setName("visible")
+                .setDescription("Response message visibility, default is true")
+                .setRequired(false)
+        )
         .setDescription("Display Warbrokers KD data for user"),
     async execute(client, interaction, version) {
-        try {
-            const authorName = interaction.user;
-            const authorID = interaction.user.id;
+        var validUser = interaction.options.getMentionable("target")?.user?.id ?? false;
+        const responseMessageVisibility = !(interaction.options.getBoolean("visible") ?? true);
 
-            const msg = await interaction.channel.send({
-                embeds: [{
-                    color: parseInt("FF0000", 16),
-                    description: "***This might take a few seconds . . .***",
-                    footer: {
-                        text: `Bot V ${version}`
-                    },
-                    timestamp: new Date(),
-                }]
-            });
+        await interaction.reply({
+            embeds: [{
+                color: parseInt("ffff00", 16),
+                title: "This might take a few seconds . . .",
+                footer: {
+                    text: `Bot V ${version}`
+                },
+                timestamp: new Date(),
+            }],
+            ephemeral: responseMessageVisibility
+        });
+
+        if (validUser) {
+            const authorID = interaction.options.getMentionable("target")?.user?.id ?? interaction.user.id;
 
             const playerDetailsObj = await manager.getPlayerDetails(authorID);
             if (!playerDetailsObj.error) {
                 const statsRow = new ActionRowBuilder()
                     .addComponents(
                         new ButtonBuilder()
-                            .setLabel(`${playerDetailsObj.gameName}`)
+                            .setLabel(`${playerDetailsObj.gameName} 的完整資訊面板`)
                             .setURL(`https://stats.warbrokers.io/players/i/${playerDetailsObj.gameID}`)
                             .setStyle(ButtonStyle.Link)
                     );
 
-                await interaction.reply({
-                    content: `${authorName}, 這是您的 KD 數據`,
+                await interaction.editReply({
                     embeds: [{
                         color: parseInt("fccbcb", 16),
                         title: `玩家名稱 : ${playerDetailsObj.gameName}`,
+                        description: `小隊 : **${playerDetailsObj.squad}**`,
                         fields: [
                             {
-                                name: ("您的 KD 值 : `" + playerDetailsObj.rounded_currentKD + "`"),
-                                value: (`您需要 \`${playerDetailsObj.rounded_neededKills}\` 次擊殺來增加 KD 值\n您可以在 KD 值下降之前死亡 \`${playerDetailsObj.rounded_neededDeaths}\` 次`),
+                                name: `您的 KD 值 : \`${playerDetailsObj.rounded_currentKD}\``,
+                                value: `您需要 \`${playerDetailsObj.rounded_neededKills}\` 次擊殺來增加 KD 值\n您可以在 KD 值下降之前死亡 \`${playerDetailsObj.rounded_neededDeaths}\` 次`,
                                 inline: true
                             },
                             {
-                                name: ("目前狀況 :"),
-                                value: (`擊殺次數 = \`${playerDetailsObj.kills}\`\n死亡次數 = \`${playerDetailsObj.deaths}\``),
-                            }
+                                name: "目前狀況 :",
+                                value: `總擊殺次數 = \`${playerDetailsObj.kills}\`\n總死亡次數 = \`${playerDetailsObj.deaths}\``,
+                            },
+                            {
+                                name: `等級 : ${playerDetailsObj.level}`,
+                                value: `\`[${playerDetailsObj.levelProgressBar}]\` ${playerDetailsObj.levelProgress}%`,
+                            },
                         ],
                         footer: {
                             text: `Bot V ${version}`
                         },
                         timestamp: new Date(),
                     }],
-                    components: [statsRow]
+                    components: [statsRow],
+                    ephemeral: responseMessageVisibility
                 });
             }
             else {
-                await interaction.reply({
+                await interaction.editReply({
                     embeds: [{
                         color: parseInt("ff0000", 16),
-                        title: "***您似乎尚未連結帳號 ?***",
-                        fields: [
-                            {
-                                name: "**連結方式 :**",
-                                value: "WBnew <頁面網址>",
-                            },
-                            {
-                                name: "**舉例 :**",
-                                value: "`A WB new https://stats.warbrokers.io/players/i/5de3a718bfea714d3b292bcb`",
-                            },
-                            {
-                                name: "***需要幫助 ?***",
-                                value: "輸入 `A WB help` 以獲得更多資訊",
-                            }
-                        ],
+                        title: "Invalid user",
                         footer: {
                             text: `Bot V ${version}`
                         },
                         timestamp: new Date(),
-                    }]
+                    }],
+                    ephemeral: responseMessageVisibility
                 });
+                // await interaction.editReply({
+                //     embeds: [{
+                //         color: parseInt("ff0000", 16),
+                //         title: "***您似乎尚未連結帳號 ?***",
+                //         fields: [
+                //             {
+                //                 name: "**連結方式 :**",
+                //                 value: "WBnew <頁面網址>",
+                //             },
+                //             {
+                //                 name: "**舉例 :**",
+                //                 value: "`A WB new https://stats.warbrokers.io/players/i/5de3a718bfea714d3b292bcb`",
+                //             },
+                //             {
+                //                 name: "***需要幫助 ?***",
+                //                 value: "輸入 `A WB help` 以獲得更多資訊",
+                //             }
+                //         ],
+                //         footer: {
+                //             text: `Bot V ${version}`
+                //         },
+                //         timestamp: new Date(),
+                //     }],
+                //     ephemeral: responseMessageVisibility
+                // });
             }
-
-            await msg.delete();
-        } catch (error) {
-            console.log(error);
+        }
+        else {
+            await interaction.editReply({
+                embeds: [{
+                    color: parseInt("ff0000", 16),
+                    title: "Invalid target user",
+                    footer: {
+                        text: `Bot V ${version}`
+                    },
+                    timestamp: new Date(),
+                }],
+                ephemeral: responseMessageVisibility
+            });
         }
     }
 };
